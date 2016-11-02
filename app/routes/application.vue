458c57f002930e1a99ff4ae66189e6c556ab6f32
“Shaun <shaunwmusic@gmail.com>
2016-11-02 17:24:15 -0500

    Fix: style to main content

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="">
-  <div class="nav">
+    <div class="nav">
       <div class="nav-left">
         <router-link to="/" class="nav-item is-brand">Puppies</router-link>
       </div>
@@ -8,19 +8,42 @@
         <router-link to="/" class="nav-item">All Puppies</router-link>
         <router-link to="/new" class="nav-item">Add Puppy</router-link>
       </div>
-  </div>
+    </div>
 
-  <div class="main">
-        <div class="container">
-        <router-view
-        :puppies="puppies"
-        :api-url="apiUrl"
-        @addPuppy="addPuppy"
-        @removePuppy="removePuppy"
-        @updatePuppy="updatePuppy">
-      </router-view>
+    <div class="main">
+      <div class="section columns">
+        <div class="column is-4">
+          <div class="panel">
+            <p class="panel-heading">Adopt a Pupper</p>
+            <div class="panel-block is-active">
+              <div class="media">
+                 <div class="media-left">
+                     <figure class="image is-64x64">
+                       <img src="http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-21.jpg" alt="">
+                     </figure>
+                  </div>
+                  <div class="">
+                     <h2 class="subtitle">Luna</h2>
+                    <router-link to="{ name: 'detail', params: { id: item.id } }"
+                    class="">
+                    read more
+                  </router-link>
+                 </div>
+               </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <router-view
+            :puppies="puppies"
+            :api-url="apiUrl"
+            @addPuppy="addPuppy"
+            @removePuppy="removePuppy"
+            @updatePuppy="updatePuppy">
+          </router-view>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -30,9 +53,6 @@ const apiUrl = 'https://tiy-tn-class-api-fall-16.herokuapp.com/puppies/shaun';
 
 
 export default {
-  components: {
-    IndexPage,
-  },
   data() {
     return {
     };
--- app/routes/index.vue
+++ app/routes/index.vue
@@ -1,46 +1,24 @@
 <template lang="html">
   <div class="">
-    <div class="section columns is-multiline">
-      <div class="panel column is-one-quarter is-paddingless">
-        <p class="panel-heading">Adopt a Pupper</p>
-        <div class="panel-block is-active">
-          <div class="media">
-             <div class="media-left">
-                 <img class="image is-64x64" src="http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-21.jpg" alt="">
-              </div>
-              <div class="">
-                 <h2 class="">Luna</h2>
-                <router-link to="{ name: 'detail', params: { id: item.id } }"
-                class="">
-                read more
-              </router-link>
-             </div>
-           </div>
-        </div>
-
-      </div>
-    </div>
-    <div class="section columns">
-      <table class="column is-half">
+    <table class="table">
       <thead>
         <tr>
           <th>Name</th>
           <th>Age</th>
         </tr>
       </thead>
-    <tbody>
-      <tr v-for="item in lunchSpots">
-        <td>{{ item.name }}</td>
-        <td>{{ item.age }}</td>
-        <td>
-          <router-link :to="{ name: 'detail', params: { id: item.id } }">
-            MORE!!!
-          </router-link>
-        </td>
-      </tr>
-    </tbody>
+      <tbody>
+        <tr v-for="item in lunchSpots">
+          <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+          <td>
+            <router-link :to="{ name: 'detail', params: { id: item.id } }">
+              MORE!!!
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
     </table>
-    </div>
   </div>
 </template>
 
