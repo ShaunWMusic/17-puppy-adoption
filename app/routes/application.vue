d8a8e15e2d1353e3dc0128e276f4285d1380dc7f
“Shaun <shaunwmusic@gmail.com>
2016-11-02 14:44:49 -0500

    Update navbar

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,5 +1,16 @@
 <template lang="html">
   <div class="app">
+  <div class="nav">
+    <div class="container">
+      <div class="nav-left">
+        <router-link to="/" class="nav-item is-brand">Puppies</router-link>
+      </div>
+      <div class="nav-right">
+        <router-link to="/" class="nav-item">All Puppies</router-link>
+        <router-link to="/new" class="nav-item">Add Puppy</router-link>
+      </div>
+    </div>
+  </div>
 
   </div>
 </template>
